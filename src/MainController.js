9fd91d4761b984581c685600806088a1748499de
pansay <jan.krepelka@gmail.com>
2015-12-25 20:25:59 +0100

    web app

--- src/MainController.js
+++ src/MainController.js
@@ -5,6 +5,7 @@
  * @ngdoc controller
  * @desc Main controller.
  * @param {Object} $scope - ng
+ * @param {Object} $sce - ng
  * @param {Object} showdown - showdown markdown converter
  * @param {Object} texts - texts constant
  * @param {Object} frenchify - frenchify
@@ -12,15 +13,27 @@
 
 module.exports = [
     '$scope',
+    '$sce',
     'showdown',
     'texts',
     'frenchify',
-    function($scope, showdown, texts, frenchify) {
+    function($scope, $sce, showdown, texts, frenchify) {
         $scope.txt = texts;
 
-        var text = '#hello, markdown!';
-        $scope.abc = showdown.makeHtml(text);
+        function convert(from, convertMarkdown, convertFrenchify) {
+            var converted = from;
+            if (convertFrenchify) {
+                converted = frenchify(converted);
+            }
+            if (convertMarkdown) {
+                converted = showdown.makeHtml(converted);
+            }
+            return converted;
+        }
 
-        $scope.def = frenchify('plop plo "bou" asdf');
+        $scope.convert = function() {
+            $scope.convert.html = convert($scope.convert.from, $scope.convert.markdown, $scope.convert.frenchify);
+            $scope.convert.html = $sce.trustAsHtml($scope.convert.html);
+        };
     }
 ];
